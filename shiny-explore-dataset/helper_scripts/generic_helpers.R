4d3a8cdc5d451e78a6e1f741a27e1a3ca5a554b5
Shane Kercheval <shane.kercheval@gmail.com>
2019-06-30 20:12:28 -0700

    Update generic_helpers.R

--- shiny-explore-dataset/helper_scripts/generic_helpers.R
+++ shiny-explore-dataset/helper_scripts/generic_helpers.R
@@ -61,68 +61,6 @@ custom_mutate <- function(dataset, factor_lump_number=NULL) {
     return (dataset)
 }
 
-easy_regression <- function(dataset,
-                            dependent_variable,
-                            independent_variables,
-                            interaction_variables=NULL,
-                            polynomial=NULL) {
-
-    if(is.null(interaction_variables)) {
-        
-        interaction_variables_formula <- ''
-    
-    } else {
-
-        interaction_variables_formula <- paste(map_chr(interaction_variables, ~ paste(., collapse =' * ')),
-                                                   collapse = ' + ')
-    }
-
-    if(is.null(independent_variables) || length(independent_variables) == 0) {
-
-        independent_variables_formula <- interaction_variables_formula
-
-    } else if(is.null(interaction_variables) || length(interaction_variables) == 0) {
-
-        independent_variables_formula <- paste(independent_variables, collapse =' + ')
-
-    } else {
-        
-        independent_variables_formula <- paste(interaction_variables_formula,
-                                               '+',
-                                               paste(independent_variables, collapse =' + '))
-    }
-
-    formula <- paste(dependent_variable, '~', independent_variables_formula)
-    
-    if(is.numeric(dataset[, dependent_variable])) {
-
-        type <- 'Linear Regression'
-        result <- lm(formula, data=dataset, na.action = na.exclude)
-        reference <- NULL
-        
-    } else {
-        
-        if(length(unique(dataset[, dependent_variable])) == 2) {
-         
-            type <- 'Logistic Regression'
-            result <- glm(formula, data=dataset, na.action = na.exclude, family=binomial)
-            reference <- rownames(contrasts(dataset[, dependent_variable]))[2]
-
-        } else {
-            
-            return (NULL)
-        }
-    }
-
-    return (
-        list(rows_excluded=which(!complete.cases(dataset[, independent_variables])),
-             type=type,
-             formula=formula,
-             results=result,
-             reference=reference)
-    )
-}
-
 capture_messages_warnings <- function(func) {
     
     messages <- list()
